66826e40d3dcfd979d0ff756f235079178676bd6
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-03-06 21:08:36 +0900

    Create user entity
    USER 테이블 기본 스펙으로 생성

---
+++ src/main/java/com/springboot/applypage/data/entity/Role.java
@@ -0,0 +1,5 @@
+package com.springboot.applypage.data.entity;
+
+public enum Role {
+    ROOT, REPR, MANA, BABY
+}
---
+++ src/main/java/com/springboot/applypage/data/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.applypage.data.repository;
+
+import com.springboot.applypage.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
