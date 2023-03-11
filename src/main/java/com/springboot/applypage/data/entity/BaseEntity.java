2b2ad8643d074b445a08431d97f57794065b62c1
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-03-11 14:00:09 +0900

    Update BaseEntity.java

--- src/main/java/com/springboot/applypage/data/entity/BaseEntity.java
+++ src/main/java/com/springboot/applypage/data/entity/BaseEntity.java
@@ -4,11 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
