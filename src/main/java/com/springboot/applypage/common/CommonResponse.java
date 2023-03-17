bfd9765e1dcd62ec567a1c002676df3da49962af
agape1225 <49804691+agape1225@users.noreply.github.com>
2023-03-17 22:52:12 +0900

    Create Sign Service

---
+++ src/main/java/com/springboot/applypage/common/CommonResponse.java
@@ -0,0 +1,46 @@
+package com.springboot.applypage.common;
+
+public enum CommonResponse {
+    SUCCESS(0, "Success"), FAIL(-1, "Fail");
+
+    int code;
+    String msg;
+
+    CommonResponse(int code, String msg){
+        this.code   = code;
+        this.msg    = msg;
+    }
+
+    public int getCode(){
+        return code;
+    }
+
+    public String getMsg(){
+        return msg;
+    }
+}
+/*package com.springboot.security.common;
+
+// 예제 13.27
+public enum CommonResponse {
+
+    SUCCESS(0, "Success"), FAIL(-1, "Fail");
+
+    int code;
+    String msg;
+
+    CommonResponse(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+}
+*/
